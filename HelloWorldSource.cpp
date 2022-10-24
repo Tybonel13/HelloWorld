d5a9c9eaeb3ee94e41905436cd75097b6ffdc990
Tybonel13 <tleblanc1301@gmail.com>
2022-10-24 15:05:05 -0400

    Add project files.

---
+++ HelloWorldSource.cpp
@@ -0,0 +1,17 @@
+/*Tyler LeBlanc
+CIS 1202.201
+Hello World
+10/24/2022*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello World!";
+
+	cout << endl << endl;
+	system("pause");
+	return(0);
+}
